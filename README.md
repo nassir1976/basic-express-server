add13816a70b347a9972ac0e5890671347d45233
nassir <daniya_n@yahoo.com>
2021-02-25 16:07:49 -0800

    lab-02

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # basic-express-server
+
+### lab- 02 
+### Autor Nassir Abegaz
+
+- <a href=https://dashboard.heroku.com/apps/nassir-server-deploy-dev class="icon fa-twitter"><span      class="label">go to heroku app </span></a>
+
+- <a href=https://github.com/nassir1976/basic-express-server class="icon fa-twitter"><span class="label">go to gitHub </span></a>
+
+<!-- - <a href=https://github.com/nassir1976/server-deployment-practice/pull/2 class="icon fa-twitter"><span class="label"> pull request </span></a> -->
+
+### Phase 1 Requirements
+- From a business requirements standpoint, we will be building a basic Express server using best practices, including server modularization, use of middleware, and tests.
+
+- Person Route
+- Method: GET
+- Path: /person
+- Expects a query string from the user with a “name” property
+- When present, output JSON to the client with this shape: { name: "name provided" }
+- Without a name in the query string, force a “500” error
+
+### Testing Requirements
+- Assert the following
+- 404 on a bad route
+- 404 on a bad method
+- 500 if no name in the query string
+- 200 if the name is in the query string
+- given an name in the query string, the output object is correct
---
+++ _test_/server.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { server } = require('../src/server.js');
+const supertest = require('supertest');
+const mockRequest = supertest(server); // supertest takes in our server so it can be started / ran during testing
+
+// describe -> test suite
+describe('**** API SERVER ****', () => {
+  Xit('should respond with a 500 on a server error', () => {
+    mockRequest.get('/person')
+    .then(results => {
+      console.log(results.status)
+      expect(results.status).toEqual(500);
+    })
+  });
+
+  it('should respond with a 404 on not found', () => {
+    mockRequest.get('/no-route')
+    .then(results => {
+      expect(results.status).toEqual(404);
+    })
+  });
+  it('should respond with a 404 on a bad method', () => {
+    mockRequest.post('/person')
+    .then(results => {
+      expect(results.status).toEqual(404);
+    })
+  });
+ describe('test 200 if the name in the quary string',()=>{
+  it('should send 200 if the name is in the query string', async () => {
+    const res = await mockRequest.get('/person?name=xyz')
+    expect(res.status).toEqual(200);
+  })
+  });
+  
+  describe('given a name in the quary string , the output object is correct',  ()=>{
+  it ('given a name in the quary string , the output object is correct', async()=>{
+    const res = await mockRequest.get('/person?name=xyz');
+    expect(res.body.name).toEqual('xyz');
+  })
+})
+})
+
